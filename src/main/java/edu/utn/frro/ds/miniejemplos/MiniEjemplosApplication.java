a1410a6487d4145d208f6d23f364c383feb9f58a
lripani <lripani@gmail.com>
2020-05-02 00:24:05 -0300

    Files Spring Initializr
    Subir files generadas por Spring Initializr

---
+++ src/main/java/edu/utn/frro/ds/miniejemplos/MiniEjemplosApplication.java
@@ -0,0 +1,13 @@
+package edu.utn.frro.ds.miniejemplos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniEjemplosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniEjemplosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/utn/frro/ds/miniejemplos/MiniEjemplosApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.utn.frro.ds.miniejemplos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniEjemplosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
