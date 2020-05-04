1a9b85a8a634b8535b255bb60fe4a22e2f5b821e
lripani <lripani@gmail.com>
2020-05-04 03:54:03 +0000

    Incre

--- src/main/java/edu/utn/frro/ds/miniejemplos/controller/ControladorEjJPQL.java
+++ src/main/java/edu/utn/frro/ds/miniejemplos/controller/ControladorEjJPQL.java
@@ -1,10 +1,16 @@
 package edu.utn.frro.ds.miniejemplos.controller;
 
+import java.util.ArrayList; // Se agrega para poder crear una collección vacía en clientes_buscarID()
+
 // ControladorEjJPQL --> Este controlador manejará las peticiones de los Ejemplos de JPQL
 // [LRI 3/5/2020 - UTDC013]
+// Modif #1 - Se agrega  clientes_buscarID()
+// [LRI 3/5/2020 - UTDC013]
+// Modif #2 Se agrega clientes_buscarCategoriaFija()
+// [LRI 3/5/2020 - UTDC013]
+
 
 import java.util.Collection;
-import java.util.ArrayList; // Se agrega para poder crear una collección vacía en clientes_buscarID()
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -31,7 +37,7 @@ public class ControladorEjJPQL {
     }
     
     @RequestMapping("/EjJPQL/clientes_buscarID") // indica el path en la url web
-	public String clientes_buscarID(Model model) {
+	public String clientes_buscarID(Model model) { // Modif #1 --> se agrega este método
         Collection<Cliente> l_colClientes ; 
         Cliente l_cte;
 
@@ -43,6 +49,18 @@ public class ControladorEjJPQL {
         model.addAttribute("sintaxisConsultaUsada", " cDao.getOne(2)");	// Se coloca por si hay que hacer algún debug
 		model.addAttribute("colClientes", l_colClientes); // Cargar en el modelo que se envía a la vista la colección de colClientess
 		return "EjJPQL/clientes-mostrar"; // nombre de la vista que se debe cargar --> cargará la página resources/templates/EjJPQL/clientes-mostrar.html
-	}
+    }
+    
+    @RequestMapping("/EjJPQL/clientes_buscarCategoriaFija") // indica el path en la url web
+	public String clientes_buscarCategoriaFija(Model model) { // Modif #2 --> se agrega este método
+        Collection<Cliente> l_colClientes;
+        
+        l_colClientes = cDao.buscarCategoriaFija(); // Recupera todos los clientes
+
+        model.addAttribute("sintaxisConsultaUsada", "@Query("SELECT c FROM Cliente c WHERE c.categoria = 'MAY'")");	// Se coloca por si hay que hacer algún debug
+		model.addAttribute("colClientes", l_colClientes); // Cargar en el modelo que se envía a la vista la colección de colClientess
+		return "EjJPQL/clientes-mostrar"; // nombre de la vista que se debe cargar --> cargará la página resources/templates/EjJPQL/clientes-mostrar.html
+    }
+    
 
 }
